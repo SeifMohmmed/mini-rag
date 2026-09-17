84e83e56e1e989aceab33f15fdc3158d81dfc49d
Seif Mohamed <saif.eissa7797@gmail.com>
2026-09-17 19:34:30 +0300

    Update file processing endpoint

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -21,16 +21,24 @@ class ProcessController(BaseController):
         file_extention = self.get_file_extentions(file_id=file_id)
         file_path = os.path.join(self.project_path,file_id)
 
+        if not os.path.exists(file_path):
+            raise None
+        
         if file_extention == ProcessingEnum.TXT.value:
             return TextLoader(file_path,encoding="utf-8")
-        elif file_extention == ProcessingEnum.PDF.value:
+        
+        if file_extention == ProcessingEnum.PDF.value:
             return PyMuPDFLoader(file_path)
-        else:
-            raise ValueError(f"Unsupported file type: {file_extention}")
+
+        return None
 
     def get_file_content(self,file_id:str):
         loader = self.get_file_loader(file_id=file_id)
-        return loader.load()
+
+        if loader:
+            return loader.load()
+        
+        return None
 
     def process_file_content(self,file_content:list,file_id:str,
                              chunk_size:int=100,overlap_size:int=20):
